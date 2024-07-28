3fd5d6fba76a414ed56f2a4e24b8fa6bf29453a2
MishaTwo <146697910+MishaTwo@users.noreply.github.com>
2024-07-28 10:45:58 +0300

    Shops

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,16 @@
 from .base import BASE, create_db
+from sqlalchemy import Column, String, Integer, Text, Boolean
+from sqlalchemy.orm import relationship
 
 class User(BASE):
-    id =
-    name=
-    email=
-    phone=
-    password = 
-    phone =
-    admin =
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name= Column(String, nullable=False, unique=True)
+    email=Column(String(100), nullable=False, unique=True)
+    phone=Column(String(30), nullable=False, unique=True)
+    password = Column(String(100), nullable=False, unique=True)
+    phone = Column(Boolean(100), nullable=False, unique=True)
+    admin = Column(Boolean(100), default=False, unique=True)
+    email_configured = Column(Boolean(100), default=False, unique=True)
+    cart = Column()
+    order = relationship("Order", buckref='customer')
